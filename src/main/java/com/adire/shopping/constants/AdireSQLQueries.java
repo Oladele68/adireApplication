b8a4668bb2fd7b744f8e9e18c224bf2685aadd61
bolataylor <bola.taylor@capgemini.com>
2020-03-13 16:41:48 +0000

    Added basic functionality to query the database and get results of adire application

--- src/main/java/com/adire/shopping/constants/AdireSQLQueries.java
+++ src/main/java/com/adire/shopping/constants/AdireSQLQueries.java
@@ -6,5 +6,9 @@ public class AdireSQLQueries {
             "b.BrandDescription , d.DesignerName, i.ImageName from Product as p, Brand as b, Designer as d, " +
             " Image as i where p.ImageID = i.ImageID and p.BrandID = b.BrandID " +
             "d.DesignerID = p.DesignerID where p.DesignerCode = ?";
+
+    public static String JDBC_CONNECTION = "jdbc:sqlserver://adire.database.windows.net:1433;" +
+            "database=adireShoppingDatabase;user=adireAdmin@adire;password=P4ssw0rd;" +
+            "encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
 }
 
