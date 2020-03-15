74cb41983d4a55713c41fa578d6020032fac3095
bolataylor <bola.taylor@capgemini.com>
2020-03-15 17:13:33 +0000

    Seperated the code into the facade, service and dao layers

--- src/main/java/com/adire/shopping/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/adire/shopping/dao/impl/ProductDaoImpl.java
@@ -2,7 +2,7 @@ package com.adire.shopping.dao.impl;
 
 import com.adire.shopping.constants.AdireSQLQueries;
 import com.adire.shopping.dao.ProductDao;
-import com.adire.shopping.entities.Product;
+import com.adire.shopping.dto.ProductDto;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -11,30 +11,34 @@ import java.util.List;
 public class ProductDaoImpl implements ProductDao {
 
     @Override
-    public List<Product> getProductDetailsByDesigner(String designerCode) throws SQLException {
-        List <Product> productList = new ArrayList<Product>();
+    public List<ProductDto> getProductDetailsByDesigner(String designerCode) throws SQLException {
+        List <ProductDto> productList = new ArrayList<ProductDto>();
         Connection connection = DriverManager.getConnection(AdireSQLQueries.JDBC_CONNECTION);
         PreparedStatement preparedStatement = connection.prepareStatement(AdireSQLQueries.PRODUCT_SQL_DESIGNERS);
         preparedStatement.setString(1, designerCode);
         ResultSet resultSet = preparedStatement.executeQuery();
             while(resultSet.next()){
 
-                Product product = new Product();
-
-                product.setProductName(resultSet.getString("ProductName"));
-
-                productList.add(product);
+                ProductDto productDto = new ProductDto();
+                productDto.setProductCode(resultSet.getString("ProductCode"));
+                productDto.setProductCode(resultSet.getString("ProductShortDesc"));
+                productDto.setProductCode(resultSet.getString("ProductLongDescription"));
+                productDto.setProductCode(resultSet.getString("ImageCode"));
+                productDto.setProductCode(resultSet.getString("BrandCode"));
+                productDto.setProductCode(resultSet.getString("DesignerCode"));
+                productDto.setProductCode(resultSet.getString("ListPrice"));
+                productList.add(productDto);
         }
         return productList;
     }
 
     @Override
-    public List<Product> getProductDetailsByStylist(String stylistCode) {
+    public List<ProductDto> getProductDetailsByStylist(String stylistCode) {
         return null;
     }
 
     @Override
-    public Product getSingleProductDetails(String productCode) {
+    public ProductDto getSingleProductDetails(String productCode) {
         return null;
     }
 }
